992472ab2d104ca6acb142715284e38dbd36d12c
Andrew Melis <andrewmelis@gmail.com>
2016-11-13 15:22:16 -0600

    add basic readme

---
+++ README.md
@@ -0,0 +1,15 @@
+nba-schedule returns all the NBA games played on an input date
+
+useful links:
+- http://data.nba.net/10s/prod/v1/today.json // linkrels
+
+- http://data.nba.net/data/10s/prod/v1/20161112/0021600129_boxscore.json
+- http://data.nba.net/data/10s/prod/v1/20161112/0021600129_mini_boxscore.json
+- http://data.nba.net/data/10s/prod/v1/20161112/0021600129_pbp_1.json
+
+- http://data.nba.net/data/10s/prod/v1/2016/players.json // ALL players
+
+- http://data.nba.net/data/json/cms/2015/league/nba_games.json // ALL games
+
+
+
--- main.go
+++ main.go
@@ -72,14 +72,3 @@ func rawGames() string {
 func scoreboardURL() string {
 	return "http://data.nba.net/data/10s/prod/v1/20161112/scoreboard.json"
 }
-
-// http://data.nba.net/data/10s/prod/v1/20161112/0021600129_boxscore.json
-// http://data.nba.net/data/10s/prod/v1/20161112/0021600129_mini_boxscore.json
-// http://data.nba.net/data/10s/prod/v1/20161112/0021600129_pbp_1.json
-
-// http://data.nba.net/data/10s/prod/v1/2016/players.json // ALL players!!!
-
-// http://data.nba.net/data/json/cms/2015/league/nba_games.json // ALL games
-
-// LINK RELATIONS
-// http://data.nba.net/10s/prod/v1/today.json
