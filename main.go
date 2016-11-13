76e5dcac5809d01924b70d8bd4a4ad0bab0fcac4
Andrew Melis <andrewmelis@gmail.com>
2016-11-13 15:21:33 -0600

    initial commit

---
+++ main.go
@@ -0,0 +1,85 @@
+// nba-schedule returns all the NBA games played on an input date
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	// fmt.Printf(rawGames())
+	// fmt.Printf(games())
+	games()
+}
+
+type Games struct {
+	Games []Game `json:"games"`
+}
+
+type Game struct {
+	Id           string    `json:"gameId"`
+	StartTime    time.Time `json:"startTimeUTC"`
+	VisitingTeam Team      `json:"vTeam"`
+	HomeTeam     Team      `json:"hTeam"`
+}
+
+type Team struct {
+	Id      string `json:"teamId"`
+	TriCode string `json:"triCode"`
+}
+
+func games() Games {
+	resp, err := http.Get(scoreboardURL())
+	if err != nil {
+		log.Fatalf("error retrieving scoreboard: %s\n", err)
+	}
+	defer resp.Body.Close()
+
+	dec := json.NewDecoder(resp.Body)
+
+	var games Games
+	for dec.More() {
+		err := dec.Decode(&games)
+		if err != nil {
+			log.Fatalf("error decoding response: %s\n", err)
+		}
+	}
+
+	fmt.Printf("%+v\n", games)
+
+	return games
+}
+
+func rawGames() string {
+	resp, err := http.Get(scoreboardURL())
+	if err != nil {
+		log.Fatalf("error retrieving scoreboard: %s\n", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("error reading scoreboard response: %s\n", err)
+	}
+
+	return string(body)
+}
+
+func scoreboardURL() string {
+	return "http://data.nba.net/data/10s/prod/v1/20161112/scoreboard.json"
+}
+
+// http://data.nba.net/data/10s/prod/v1/20161112/0021600129_boxscore.json
+// http://data.nba.net/data/10s/prod/v1/20161112/0021600129_mini_boxscore.json
+// http://data.nba.net/data/10s/prod/v1/20161112/0021600129_pbp_1.json
+
+// http://data.nba.net/data/10s/prod/v1/2016/players.json // ALL players!!!
+
+// http://data.nba.net/data/json/cms/2015/league/nba_games.json // ALL games
+
+// LINK RELATIONS
+// http://data.nba.net/10s/prod/v1/today.json
