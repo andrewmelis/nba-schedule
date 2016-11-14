a416ef665e8e961d7a5ac776784dc4693687da1e
Andrew Melis <andrewmelis@gmail.com>
2016-11-13 18:20:38 -0600

    working spaghetti version

--- main.go
+++ main.go
@@ -1,19 +1,18 @@
-// nba-schedule returns all the NBA games played on an input date
+// nba-schedule returns all the NBA games played on an input date //TODO proper
 package main
 
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"time"
 )
 
 func main() {
-	// fmt.Printf(rawGames())
-	// fmt.Printf(games())
-	games()
+	http.HandleFunc("/games", GamesHandler)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 type Games struct {
@@ -25,6 +24,7 @@ type Game struct {
 	StartTime    time.Time `json:"startTimeUTC"`
 	VisitingTeam Team      `json:"vTeam"`
 	HomeTeam     Team      `json:"hTeam"`
+	Period       Period    `json:"period"`
 }
 
 type Team struct {
@@ -32,10 +32,40 @@ type Team struct {
 	TriCode string `json:"triCode"`
 }
 
-func games() Games {
-	resp, err := http.Get(scoreboardURL())
+type Period struct {
+	Current int `json:"current"`
+}
+
+func GamesHandler(w http.ResponseWriter, r *http.Request) {
+	games, err := GetGames()
 	if err != nil {
-		log.Fatalf("error retrieving scoreboard: %s\n", err)
+		log.Printf("error retrieving games: %s\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintln(w, `{"error":"server error occurred"}`)
+		return
+	}
+
+	enc := json.NewEncoder(w)
+	err = enc.Encode(&games)
+	if err != nil {
+		log.Printf("error encoding games: %s\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintln(w, `{"error":"server error occurred"}`)
+		return
+	}
+}
+
+func GetGames() (Games, error) {
+	scoreboardURL, err := TodayScoreboardURL()
+	if err != nil {
+		log.Printf("error retrieving scoreboard url: %s\n", err)
+		return Games{}, err
+	}
+
+	resp, err := http.Get(scoreboardURL)
+	if err != nil {
+		log.Printf("error retrieving scoreboard: %s\n", err)
+		return Games{}, err
 	}
 	defer resp.Body.Close()
 
@@ -45,30 +75,42 @@ func games() Games {
 	for dec.More() {
 		err := dec.Decode(&games)
 		if err != nil {
-			log.Fatalf("error decoding response: %s\n", err)
+			log.Printf("error decoding response: %s\n", err)
+			return Games{}, err
 		}
 	}
 
-	fmt.Printf("%+v\n", games)
-
-	return games
+	return games, nil
 }
 
-func rawGames() string {
-	resp, err := http.Get(scoreboardURL())
+type NBATodayResponse struct {
+	Links map[string]string `json:"links"`
+}
+
+func TodayScoreboardURL() (string, error) {
+	const NBABaseURL = "http://data.nba.net"
+	const NBATodayRoute = "/10s/prod/v1/today.json"
+
+	NBATodayURL := fmt.Sprintf("%s%s", NBABaseURL, NBATodayRoute)
+
+	resp, err := http.Get(NBATodayURL)
 	if err != nil {
-		log.Fatalf("error retrieving scoreboard: %s\n", err)
+		log.Printf("error retrieving today url: %s\n", err)
+		return "", err
 	}
 	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalf("error reading scoreboard response: %s\n", err)
+	dec := json.NewDecoder(resp.Body)
+	var todayResp NBATodayResponse
+
+	for dec.More() {
+		err := dec.Decode(&todayResp)
+		if err != nil {
+			log.Printf("error decoding response: %s\n", err)
+			return "", err
+		}
 	}
 
-	return string(body)
-}
-
-func scoreboardURL() string {
-	return "http://data.nba.net/data/10s/prod/v1/20161112/scoreboard.json"
+	NBATodayScoreboardURL := fmt.Sprintf("%s%s", NBABaseURL, todayResp.Links["todayScoreboard"])
+	return NBATodayScoreboardURL, nil
 }
